462132f539312a371e9a161410f08646a1c99947
Yash Viradia <yashviradia@ylapahviotp.local>
2023-05-26 08:39:00 +0200

    Completed Part 2: added CardList component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
-import Card from '../components/Card';
+import CardList from '../components/CardList';
 import './App.css';
 import { robots } from '../robots';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-                <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-                <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-            </div>
+            <CardList robots={robots} />
         )
     }
 }
