0b0f51d6ba790b24e657281da61f3ec01560a702
Yash Viradia <yashviradia@ylapahviotp.local>
2023-05-27 06:54:22 +0200

    completed Part 3: added state and made searchbox component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
 import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
 import './App.css';
 import { robots } from '../robots';
 
+
+
 class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+        console.log(event.target.value);
+        
+    }
+
     render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
         return (
-            <CardList robots={robots} />
+            <div className='tc'>
+                <h1>Robofriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filteredRobots} />
+            </div>
         )
     }
 }
