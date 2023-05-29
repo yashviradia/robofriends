05dc5f3a75017354732edc4b7b487abd5618b2c1
Yash Viradia <yashviradia@ylapahviotp.local>
2023-05-29 19:28:05 +0200

    completed Part 4: added API call and used lifecycle components

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import './App.css';
-import { robots } from '../robots';
-
 
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.cypress.io/users")
+            .then(response => {
+                return response.json();
+            })
+            .then(users => {
+                this.setState({ robots: users})
+            });
     }
 
     onSearchChange = (event) => {
@@ -25,13 +33,17 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return (
-            <div className='tc'>
-                <h1>Robofriends</h1>
-                <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredRobots} />
-            </div>
-        )
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1>Robofriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList robots={filteredRobots} />
+                </div>
+            );
+        }
     }
 }
 
